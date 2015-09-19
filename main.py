7172bd33cb48327bad1da76b57e8ed7542c4a00c
ngul <negul@microsoft.com>
2015-09-19 21:10:18 +0100

    fixing the app

--- main.py
+++ main.py
@@ -12,25 +12,32 @@ app.debug = True
 def get_prayer_times():
     r = requests.get('http://www.eastlondonmosque.org.uk/')
     soup = BeautifulSoup(r.content)
-    current_prayer = soup.findAll(True, {'class':['current']})
-    fajr = current_prayer[0].contents[3].contents[0].get_text()
-    sunrise = current_prayer[0].contents[3].contents[2].get_text()
-    duhr = current_prayer[0].contents[5].contents[0].get_text()
-    asr = current_prayer[0].contents[7].contents[0].get_text()
-    maghrib = current_prayer[0].contents[9].contents[0].get_text()
-    isha = current_prayer[0].contents[11].contents[0].get_text()
+    current_prayer = soup.findAll('div', attrs={'class':'salah-time-row'})[2]
+    fajr = current_prayer.contents[-10].get_text()
+    #sunrise = current_prayer.contents[-8].get_text()
+    duhr = current_prayer.contents[-8].get_text()
+    asr = current_prayer.contents[-6].get_text()
+    maghrib = current_prayer.contents[-4].get_text()
+    isha = current_prayer.contents[-2].get_text()
+
+    if int(duhr.split(':')[0]) == 11:
+      duhr = duhr + " AM"
+    else:
+      duhr = duhr + " PM"
+      
 
     prayer_json = {
-                    "fajr": fajr,
-                    "sunrise": sunrise,
+                    "fajr": fajr + " AM",
+                    #"sunrise": sunrise,
                     "duhr": duhr,
-                    "asr": asr,
-                    "maghrib": maghrib,
-                    "isha": isha
+                    "asr": asr + " PM",
+                    "maghrib": maghrib + " PM",
+                    "isha": isha + " PM"
                   }
 
     return flask.jsonify(**prayer_json)
             
-
+if __name__ == "__main__":
+      app.run()
 
 
